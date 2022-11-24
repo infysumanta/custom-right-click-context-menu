ca1fbddf897aca4d6ad2a9a1cea3b577a7873113
Sumanta Kabiraj <sumantablog@gmail.com>
2022-11-24 18:58:23 +0530

    ready for deployment

---
+++ script.js
@@ -0,0 +1,29 @@
+const contextMenu = document.querySelector(".wrapper");
+const shareMenu = document.querySelector(".share-menu");
+
+document.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+  let x = e.offsetX;
+  let y = e.offsetY;
+  winWidth = window.innerWidth;
+  winHeight = window.innerHeight;
+  cmWidth = contextMenu.offsetWidth;
+  cmHeight = contextMenu.offsetHeight;
+
+  if (x > winWidth - cmWidth - shareMenu.offsetWidth) {
+    shareMenu.style.left = "-200px";
+  } else {
+    shareMenu.style.left = "";
+    shareMenu.style.right = "-200px";
+  }
+  x = x > winWidth - cmWidth ? winWidth - cmWidth : x;
+  y = y > winHeight - cmHeight ? winHeight - cmHeight : y;
+
+  contextMenu.style.left = `${x}px`;
+  contextMenu.style.top = `${y}px`;
+  contextMenu.style.visibility = "visible";
+});
+
+document.addEventListener("click", () => {
+  contextMenu.style.visibility = "hidden";
+});
